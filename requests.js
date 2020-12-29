9e525706c6e6c1891fc5c0c7da85f666f8e0f04a
Jordan Kurtzman <jordankurtzman@jordans-mbp-2.attlocal.net>
2020-12-28 16:02:23 -0800

    Initial Commit

---
+++ requests.js
@@ -0,0 +1,28 @@
+const searchBeerByName = async (beerName) => {
+    const response = await fetch(`https://api.punkapi.com/v2/beers?beer_name=${beerName}`)
+    if(response.status === 200){
+        return data = response.json()   
+    }else throw new Error('Unable to fetch data')
+}
+
+const searchBeerByFoodPairing = async (foodName) => {
+    const response = await fetch(`https://api.punkapi.com/v2/beers?beer_name=${foodName}`)
+    if (response.status === 200) {
+        return data = response.json()
+    } else throw new Error('Unable to fetch data')
+}
+
+const searchMellowBeers = async() => {
+    const response = await fetch(`https://api.punkapi.com/v2/beers?ibu_lt=20`)
+    if (response.status === 200) {
+        return data = response.json()
+    } else throw new Error('Unable to fetch data')
+}
+
+const searchBitterBeers = async () => {
+    const response = await fetch(`https://api.punkapi.com/v2/beers?ibu_gt=70`)
+    if (response.status === 200) {
+        return data = response.json()
+    } else throw new Error('Unable to fetch data')
+}
+
